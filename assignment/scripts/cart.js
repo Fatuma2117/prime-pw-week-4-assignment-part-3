478d73748707b975357b8fc3fe8ab9cd347dc5d7
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-03-15 05:36:50 -0500

    Rough draft

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -37,10 +37,37 @@ console.log(`Basket is now ${basket}`);
 // Stretch Goals
 
  const maxItems = 5
-
- function isFull(). (params) {
-     
+ function isFull() {
+     if (basket< maxItems) {
+         return false;
+     } else if (basket >= maxItems) {
+         return true
+     }
  }
+ console.log('Expect true:', isFull())
+ isFull()
 
 //- return `false` if the basket contains *less* than max number of items
-- return `true` otherwise (equal or more than maxItems)
+// - return `true` otherwise (equal or more than maxItems)
+
+// Update the required `addItem` function to:
+//   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
+//   - If an item was added to the array, return `true`
+//   - If there was no room and the item could not be added return `false`
+
+function addItem(item) {
+    basket.push(26);
+    return true;  
+}
+console.log('Expect true', addItem());
+addItem(26);
+
+
+/////
+
+// Create a function called `removeItem`. It should:
+// - Take an input parameter for a string `item`
+// - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf) to find the index of the first matching item in the basket.
+// - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to remove the first matching item from the basket.
+// - Return the item removed or `null` if the item was not found
+
