c3fc4518e9f356479685f74be57fa96d5e5f0cc2
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-03-25 15:36:28 -0500

    Final resubmission

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,38 +8,49 @@ let basket =[];
 //   - take an input parameter for a string `item`
 //   - add the new item to the global array `basket`. 
 //   - return `true` indicating the item was added
+
 function addItem(item) {
-    basket.push('grapes');
+    basket.push(item);
     return true;  
 }
-console.log('Expect true', addItem());
-// addItem(26);
+
+addItem('candy')
+addItem('cake')
+console.log(basket);
+console.log('Expect true:', addItem('candy'))
+
 
 // Create a function called `listItems`. It should:
 //   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
+
+
 function listItems(item){
-    for (let i = 0; i < basket.length; i++) {
-console.log(basket[i]);
+    for (let item of basket) {
+        console.log(item);
     }     
  }
-console.log(listItems());
-console.log(listItems(basket));
+
+ listItems();
+
+// console.log(listItems());
     
 //  - Create a function called `empty`. It should:
 //   - reset the `basket` to an empty array
-let reset = []
+
 function empty() {
-    basket = reset
-    return basket
+    basket = [] 
 }
-console.log(empty());
+
+empty();
+console.log('Expect []:', basket);
 
 
 // Example Below
-console.log(`Basket is ${basket}`);
-console.log('Adding apples (expect true)', addItem('apples'));
-console.log(`Basket is now ${basket}`);
+// console.log(`Basket is ${basket}`);
+// console.log('Adding apples (expect true)', addItem('apples'));
+// console.log(`Basket is now ${basket}`);
+
 
 // Stretch Goals
 
@@ -52,7 +63,7 @@ console.log(`Basket is now ${basket}`);
          return true;
      }
  }
- console.log('Expect true:', isFull())
+//  console.log('Expect true:', isFull())
  isFull();
 
 //- return `false` if the basket contains *less* than max number of items
